cd5f655e74ced13dde01e5958730651431355bd2
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-01-28 16:35:04 +0200

    Implemented MobileNavigation

---
+++ src/components/Navigation/MobileNavigation.jsx
@@ -0,0 +1,25 @@
+import { NavLinks } from "./NavLinks";
+import styles from "./Navigation.module.css";
+import hamburgerIcon from '../../assets/burger-menu-icon.svg';
+import closeIcon from "../../assets/close-menu-icon.svg";
+import { useState } from "react";
+
+export const MobileNavigation = () =>{
+const [showLinks, setShowLinks] = useState(false);
+
+const ShowLinks = () => {
+  console.log(showLinks)
+  setShowLinks(!showLinks)
+}
+
+const hamburger =  <img src={hamburgerIcon} alt="Open menu" className={styles["hamburger-icon"]} onClick={ShowLinks}/>
+const closeMenu = <img src={closeIcon} alt="Close menu" className={styles["hamburger-icon"]} onClick={ShowLinks}/>
+
+     return(
+      <nav className={styles["nav-items-mobile"]}>
+      <h2 className={styles.logo}>Logo Mobile</h2>
+      {showLinks ? closeMenu : hamburger}
+      {showLinks && <NavLinks />}
+  </nav>
+     )
+ }
