0dd138417d557d29d1be2dddea26a79075f9d327
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-01-28 16:34:41 +0200

    Implemented NavLinks

---
+++ src/components/Navigation/NavLinks.jsx
@@ -0,0 +1,22 @@
+import styles from "./Navigation.module.css";
+
+export const NavLinks = () =>{
+  return(
+    <div className={styles["nav-options"]}>
+      <ul>
+          <li>
+              <a href="/">Home</a>
+          </li>
+          <li>
+              <a href="/#services">About</a>
+          </li>
+          <li>
+              <a href="/#about">Projects</a>
+          </li>
+          <li>
+              <a href="/#contact">Contacts</a>
+          </li>
+      </ul>
+      </div>
+  )
+}
