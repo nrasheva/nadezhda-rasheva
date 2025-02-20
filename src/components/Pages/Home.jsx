bf8677fc71faa6e8c220660b79d3cbc34100e64d
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-20 17:52:03 +0200

    Updated Home

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -13,26 +13,53 @@ export const Home = () => {
   }
 
   return (
-     <div className={styles["home-main"]}>
+    <div id="home" className={styles["home-main"]}>
       <div className={styles["home-content"]}>
         <div className={styles["home-left-content"]}>
-        <div className={styles["home-text"]}>
-        <h3>Hello<span>.</span></h3>
-        <p className={styles["shifted-text"]}><span>_______________________________</span> I am Nadya</p>
-        <h3>Web developer</h3>
-        </div>
-        <div className={styles["home-buttons"]}>
-          <MainButton text="Got a project" onClick={ContactClick} className={buttonStyles["filled-button"]}/>
-          <MainButton text="My resume" onClick={DownloadResume} className={buttonStyles["border-button"]} />
-        </div>
+          <div className={styles["home-text"]}>
+            <h3>
+              Hello<span>.</span>
+            </h3>
+            <p className={styles["shifted-text"]}>
+              <span>_______________________________</span> I am Nadya
+            </p>
+            <h3>Web developer</h3>
+          </div>
+          <div className={styles["home-buttons"]}>
+            <MainButton
+              text="Got a project"
+              onClick={ContactClick}
+              className={buttonStyles["filled-button"]}
+            />
+            <MainButton
+              text="My resume"
+              onClick={DownloadResume}
+              className={buttonStyles["border-button"]}
+            />
+          </div>
         </div>
         <div className={styles["home-photo"]}>
-          Text
+          <div className={styles["photo-field"]}></div>
         </div>
       </div>
       <div className={styles["home-line"]}>
-      Text
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <ul>
+          <li>C#</li>
+          <li>React</li>
+          <li>React-Native</li>
+          <li>Angular</li>
+        </ul>
+        <ul>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Nest.js</li>
+        </ul>
       </div>
-      </div>
-  ) 
+    </div>
+  ); 
 };
