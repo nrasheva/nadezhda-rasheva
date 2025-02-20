92b1a3238f9bbea5e4b41587413671df52828e04
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-20 17:51:30 +0200

    Updated Desktop nav

--- src/components/Navigation/DesktopNavigation.jsx
+++ src/components/Navigation/DesktopNavigation.jsx
@@ -1,11 +1,27 @@
 import { NavLinks } from "./NavLinks";
+import { useState, useEffect } from "react";
 import styles from "./Navigation.module.css";
 
 export const DesktopNavigation = () =>{
-     return(
-      <nav className={styles["nav-items-desktop"]}>
-      <h2 className={styles.logo}>Nadezhda Rasheva</h2>
-      <NavLinks />
-  </nav>
-     )
+    const [navBg, setNavBg] = useState(false);
+
+    const changeNavBackground = () => {
+        setNavBg(window.scrollY >= 100);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", changeNavBackground);
+        return () => {
+            window.removeEventListener("scroll", changeNavBackground);
+        };
+    }, []);
+
+     return (
+        <nav className={navBg ? styles.navScrolled : styles["nav-items-desktop"]}>
+         <h2 className={styles.logo}>
+           Nadya R<span>.</span>
+         </h2>
+         <NavLinks />
+       </nav>
+     );
  }
