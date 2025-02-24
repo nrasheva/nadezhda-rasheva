0c903bab9157d153b5d6e65e730b2e7d491fe418
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-24 15:12:00 +0200

    Updated Card

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,24 +1,27 @@
 import styles from "./Card.module.css";
 import linkIcon from "../../assets/link-icon.svg";
+import gitIcon from "../../assets/git-icon.svg";
 
-export const Card = ({text, src, onClick, className}) => {
+export const Card = ({ text, title, src, onClick, onClickTitle, onClickGit, className }) => {
+  
   return (
-    <div
-      className={`${styles["card-main"]} ${className || ""}`}
-      onClick={onClick}
-    >
+    <div className={`${styles["card-main"]} ${className || ""}`}>
       <div className={styles["card-image"]}>
-      <img src={src}/>
+        <img src={src} />
       </div>
       <div className={styles["card-description"]}>
         <div className={styles["card-text"]}>
           <hr />
-          <span>{text}</span>
+          <span>
+            <p onClick={onClickTitle}>{title}</p>
+            <p>{text}</p>
+          </span>
         </div>
         <div className={styles["card-icon"]}>
-          <img src={linkIcon} alt="Link" />
+          <img src={linkIcon} alt="Link" onClick={onClick} />
+          <img src={gitIcon} alt="GitHub" onClick={onClickGit} />
         </div>
       </div>
     </div>
-  ); 
+  );
 };
