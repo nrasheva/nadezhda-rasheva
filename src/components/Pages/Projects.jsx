04cf5146cea20db6fe2a81bb0849f4a5ee78ad31
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-24 15:11:44 +0200

    Updated Projects

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,5 +1,8 @@
 import styles from "./Projects.module.css";
 import arrowIcon from "../../assets/arrow-down.svg";
+import moviemates from "../../assets/moviemates.png";
+import breezy from "../../assets/breezy.png";
+
 import { Card } from "../UI/Card";
 import { useRef } from "react";
 
@@ -10,6 +13,14 @@ export const Projects = () => {
     projectsRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+  const OpenLink = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const OpenGitLink = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div id="projects" className={styles["projects-main"]}>
       <div className={styles["projects-title"]}>
@@ -17,8 +28,10 @@ export const Projects = () => {
         <img src={arrowIcon} alt="Down" onClick={GoToProjects} />
       </div>
       <div ref={projectsRef} className={styles["projects-content"]}>
-        <Card />
-        <Card />
+        <Card title="Moviemates" text="(React, Redux, JavaScript, HTML, CSS, Node.js, Express, Mongo, Mongoose)" src={moviemates} onClick={() => OpenLink("https://moviemates-website.onrender.com/discover")} 
+          onClickTitle={() => OpenGitLink("https://github.com/nrasheva/moviemates-website")}  onClickGit={() => OpenGitLink("https://github.com/nrasheva/moviemates-api")}  />
+        <Card title="Breezy" text="(Angular, JavaScript, HTML, CSS, Node.js, Express, Mongo, Mongoose)" src={breezy} onClick={() => OpenLink("https://breezy-website.onrender.com/#/air-quality")} 
+          onClickTitle={() => OpenGitLink("https://github.com/nrasheva/breezy-website")}  onClickGit={() => OpenGitLink("https://github.com/nrasheva/breezy-api")} />
         <Card />
         <Card />
         <Card />
