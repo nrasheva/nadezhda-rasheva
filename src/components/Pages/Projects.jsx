42e1e3fd90d4c66de83b8b8cc732cd977453806a
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-25 12:51:31 +0200

    Updated Projects

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -2,12 +2,17 @@ import styles from "./Projects.module.css";
 import arrowIcon from "../../assets/arrow-down.svg";
 import moviemates from "../../assets/moviemates.png";
 import breezy from "../../assets/breezy.png";
+import emerald from "../../assets/emerald.png";
+import lla from "../../assets/lla.png";
+import sampleHub from "../../assets/sampleHub.png";
 
 import { Card } from "../UI/Card";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export const Projects = () => {
   const projectsRef = useRef(null);
+  const privateMessageRef = useRef(null); 
+  const [emptyLink, setEmptyLink] = useState(false);
 
   const GoToProjects = () => {
     projectsRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -18,6 +23,12 @@ export const Projects = () => {
   };
 
   const OpenGitLink = (url) => {
+    if(url == "") {
+      setEmptyLink(true);
+      setTimeout(() => setEmptyLink(false), 5000);
+      privateMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      return;
+    }
     window.open(url, "_blank");
   };
 
@@ -25,16 +36,20 @@ export const Projects = () => {
     <div id="projects" className={styles["projects-main"]}>
       <div className={styles["projects-title"]}>
         <h2>Projects</h2>
-        <img src={arrowIcon} alt="Down" onClick={GoToProjects} />
+        <img ref={privateMessageRef} src={arrowIcon} alt="Down" onClick={GoToProjects} />
       </div>
+      {emptyLink && <p style={{color: "var(--secondary)"}}>This project is private. You can only review the web source.</p>}
       <div ref={projectsRef} className={styles["projects-content"]}>
         <Card title="Moviemates" text="(React, Redux, JavaScript, HTML, CSS, Node.js, Express, Mongo, Mongoose)" src={moviemates} onClick={() => OpenLink("https://moviemates-website.onrender.com/discover")} 
           onClickTitle={() => OpenGitLink("https://github.com/nrasheva/moviemates-website")}  onClickGit={() => OpenGitLink("https://github.com/nrasheva/moviemates-api")}  />
         <Card title="Breezy" text="(Angular, JavaScript, HTML, CSS, Node.js, Express, Mongo, Mongoose)" src={breezy} onClick={() => OpenLink("https://breezy-website.onrender.com/#/air-quality")} 
           onClickTitle={() => OpenGitLink("https://github.com/nrasheva/breezy-website")}  onClickGit={() => OpenGitLink("https://github.com/nrasheva/breezy-api")} />
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Emerald" text="(JavaScript, HTML, CSS, C#)" src={emerald} onClick={() => OpenLink("https://emerald.beyonddata.bg/")} 
+          onClickTitle={() => OpenGitLink("")}  onClickGit={() => OpenGitLink("")}  />
+        <Card title="LLA score" text="(JavaScript, HTML, CSS, C#)" src={lla} onClick={() => OpenLink("https://llascore.beyonddata.bg/")} 
+          onClickTitle={() => OpenGitLink("")}  onClickGit={() => OpenGitLink("")}  />
+         <Card title="SampleHub" text="(JavaScript, HTML, CSS, C#)" src={sampleHub} onClick={() => OpenLink("https://samplehub.ie/")} 
+          onClickTitle={() => OpenGitLink("")}  onClickGit={() => OpenGitLink("")}  />
         <Card />
       </div>
     </div>
