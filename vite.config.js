80b2f9287ed9002d91c309e6c731308781b77e9e
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-02-26 12:08:49 +0200

    Added compress

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compress from 'vite-plugin-compress';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // Enable the compress plugin for Gzip or Brotli compression
+    compress({
+      verbose: true, // Enable verbose output for compression details
+      disable: false, // Set to true to disable compression
+      threshold: 10240, // Minimum file size (in bytes) for compression (default is 10240 bytes)
+      algorithm: 'brotli', // You can choose 'gzip', 'brotli', or 'deflate' (use 'brotli' for better compression)
+      ext: '.br', // Extension for Brotli compressed files, can be '.gz' for gzip
+    }),
+  ],
+  assetsInclude: ['**/*.bmp'], // This includes the BMP file format as assets
+});
